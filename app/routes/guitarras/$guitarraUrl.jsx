b5b84799aada161aa73d37292731b714cb5f4158
JoeManuel15 <jsmnlhm@outlook.com>
2023-03-05 19:56:28 -0600

    Se comenzo con la seccion del carrito

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useLoaderData } from "react-router"
 import { getGuitarra } from '~/models/guitarras.server'
 
@@ -31,8 +32,26 @@ export function meta({data}) {
 
 function Guitarra() {
 
+    const [ cantidad, setCantidad ] = useState(0)
     const guitarra = useLoaderData()
     const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if(cantidad < 1) {
+            alert('Debes seleccionar una cantidad')
+            return
+        }
+
+        const guitarraSeleccionada = {
+            id: guitarra.data[0].id,
+            imagen: imagen.data.attributes.url,
+            nombre,
+            precio,
+            cantidad
+        }
+    }
     
     return (
         <div className="guitarra">
@@ -42,6 +61,27 @@ function Guitarra() {
                 <h3>{nombre}</h3>
                 <p className="texto">{descripcion}</p>
                 <p className="precio">{precio}</p>
+
+                <form onSubmit={handleSubmit} className="formulario">
+                    <label htmlFor="cantidad">Cantidad</label>
+
+                    <select 
+                        onChange={ e => setCantidad(+e.target.value) }
+                        id="cantidad"
+                    >
+                        <option value="0">-- Seleccione --</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+
+                    <input
+                        type="submit"
+                        value="Agregar al carrito"
+                    />
+                </form>
             </div>
         </div>
     )
